5f8ea3f590668efd699021274a8aa64a5e33e2f3
sandiprj <radadiyasandip89@gmail.com>
2023-03-23 11:59:42 +0530

    new chnages

--- block_activities.php
+++ block_activities.php
@@ -3,6 +3,7 @@
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
 // Moodle is distributed in the hope that it will be useful,
